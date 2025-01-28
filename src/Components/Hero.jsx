c8e68f1a3a2995b345386a051f5a9e475686c567
RokRask <rokas.raskevic@gmail.com>
2025-01-28 19:18:09 +0200

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,14 +4,17 @@ import leaves2 from '../Images/leaves2.png';
 
 export default function Hero() {
     return (
-        <div className='hero'>
-            <div className='wrapper hero-card'>
-                <h1>Plants are our Passion</h1>
-                <p>Even if you don’t have a green thumb, you can still have a green home.</p>
-                <button className='wrapper'>GET PLANTING</button>
+        <>
+            <div className='hero'>
+                <div className='wrapper hero-card'>
+                    <h1>Plants are our Passion</h1>
+                    <p>Even if you don’t have a green thumb, you can still have a green home.</p>
+                    <button className='wrapper'>GET PLANTING</button>
+                </div>
+                <img src={leaves1} alt="tree leaves" />
+                <img src={leaves2} alt="tree leaves" />
             </div>
-            <img src={leaves1} alt="tree leaves" />
-            <img src={leaves2} alt="tree leaves" />
-        </div>
+            <div className='green-border'></div>
+        </>
     )
 }
