75eba2b7b92c3674395f16f63de8551077ffa79a
RokRask <rokas.raskevic@gmail.com>
2025-01-30 20:19:02 +0200

    commit

---
+++ src/Components/Button.jsx
@@ -0,0 +1,10 @@
+import './ComponentStyles/Button.css';
+
+export default function Button() {
+
+    return (
+        <div className='wrapper'>
+            <button className='hero-button'>GET PLANTING</button>
+        </div>
+    )
+}
