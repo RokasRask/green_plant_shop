5cedd133009a58d6761e4d35cd42f190a394aedc
RokRask <rokas.raskevic@gmail.com>
2025-01-31 14:01:05 +0200

    commit

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,6 +1,15 @@
 import './ComponentStyles/Title.css';
-export default function Title({ text1, text2 }) {
+export default function Title({ text1, text2, showLink }) {
     return (
-        <h2 className='sectionTitle'><span className='greenTitle'>{text1}</span><span>{text2}</span></h2>
+        <div className='title-box'>
+            <h2 className='sectionTitle'>
+                <span className='greenTitle'>{text1}</span>
+                <span>{text2}</span>
+            </h2>
+ 
+            {showLink && (
+                <a href='#' className='title-link'>Shop all &gt;</a>
+            )}
+        </div>
     )
 }
